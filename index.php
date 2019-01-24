2a59cf6120eb891331a588f930700a92e40a933a
イイダリョウ <iidazz0109@gmail.com>
2019-01-24 22:18:23 +0900

    フォームの中身を作成

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<h1>フォームテスト</h1>
-<p>formの動作が知りたくて自作</p>
-<form></form>
+<h1>クリエイター川柳</h1>
+<p>今日の一句：</p>
+<form method="post" action="">
+	<input type="texxt" name="ikku">
+	<input type="submit" name="yomu" value="詠む">
+</form>
 <p>by <a href="https://www.i-ryo.com" target="_blank">イイダリョウ</a></p>
