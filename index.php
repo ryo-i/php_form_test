4bedc32c434db4550abb4ff1f92bd1d67b17cc13
イイダリョウ <iidazz0109@gmail.com>
2019-01-24 22:45:40 +0900

    head要素等の追加、pタグの見出し化け、セクショニング

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>クリエイター川柳</title>
+</head>
+<body>
+<article>
 <h1>クリエイター川柳</h1>
-<p>今日の一句：</p>
+<section class="today_ikku">
+<h2>今日の一句：</h2>
 <form method="post" action="">
 	<input type="texxt" name="ikku">
 	<input type="submit" name="yomu" value="詠む">
 </form>
-
+</section>
+<section class="your_ikku">
+<h2>あなたの一句</h2>
 <p><?php 
 $ikku = $_POST["ikku"];
 echo $ikku;	
 ?></p>
+</section>
 <p>by <a href="https://www.i-ryo.com" target="_blank">イイダリョウ</a></p>
 
+</article>
+</body>
+</html>
